6a5dddf8042a1ae568fd4f7b870155c4a91bf7e4
GreyShinobi2013 <getachewnatnael55@gmail.com>
2022-12-13 05:05:41 +0300

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,10 +10,9 @@ int main(void)
 {
 	int numbers;
 
-	for (numbers = 0; numbers <= 10; numbers++)
+	for (numbers = 0; numbers < 10; numbers++)
 	{
 	printf("%d", numbers);
-	
 	}
 	printf("\n");
 	return (0);
