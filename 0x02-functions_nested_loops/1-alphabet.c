ea2b1f9e1150b952709061e1ad9a12829e8170d6
GreyShinobi2013 <getachewnatnael55@gmail.com>
2022-12-15 02:45:30 +0300

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
-	print_alphabet();
-	return (0);
+void print_alphabet(void){
+	int ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+        _putchar(ch);
+        }
+        _putchar('\n');
+
 }
 
