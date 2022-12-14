ef5a427f9ed26c33961e2fa504850feaade8f54c
GreyShinobi2013 <getachewnatnael55@gmail.com>
2022-12-14 04:29:29 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,15 +9,14 @@
 
 int main(void)
 {
-	int count;
 	char message[] = "_putchar";
-    
-    for (count = 0; count < 8; count++)
-    {
+	
+	for (int count = 0; count <8 ; count++)
+	{
 	if (message[count] == '\0')
 	{
-		_putchar('\n');
-		break;
+	_putchar('\n');
+	break;
 	}
 	else
 		_putchar(message[count]);
