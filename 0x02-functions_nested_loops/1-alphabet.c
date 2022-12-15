1d1d1c6bd1363662e0795b4b355e9949091079a7
GreyShinobi2013 <getachewnatnael55@gmail.com>
2022-12-15 03:15:24 +0300

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
+ * print_alphabet - prints lowercase letters form a to z
+ *
  * main - Entry point
  *
- * 
+ * Return: Always 0 (success)
  */
+
 
 void print_alphabet(void)
 {
