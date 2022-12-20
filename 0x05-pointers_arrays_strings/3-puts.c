d9767b24c6412e85ff308dd6810e5a9d36324d07
GreyShinobi2013 <getachewnatnael55@gmail.com>
2022-12-20 18:53:11 +0300

    prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,7 +5,7 @@
  *
  *Description:  prints a string followed by a new line
  *
- *@param - Accepts String input
+ *@param str is a pointer that points to a pointer variable str in the main function
  *
  *Return: nothing(successful)
  */
