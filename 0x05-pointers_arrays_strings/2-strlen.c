4cafbc2372f7e918ed45d75290712adbc7bfc3ff
GreyShinobi2013 <getachewnatnael55@gmail.com>
2022-12-20 04:01:03 +0300

    legth of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,15 @@
 #include "main.h"
-/*
- *_strlen - Entry point
+/**
+ * _strlen - Entry point
  *
- *@s: input
+ * @s: input
  *
  * Return: Always 0(success)
- * */
+ */
 
 int _strlen(char *s)
 {
-
-	_putchar(strlen(s));
+	int length = strlen(*s);
+	_putchar(length);
 	return (0);
 }
