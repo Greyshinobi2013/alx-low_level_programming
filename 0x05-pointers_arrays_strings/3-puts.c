a592d526a91220f1e74ca15e3efea076f4c9b6ab
GreyShinobi2013 <getachewnatnael55@gmail.com>
2022-12-20 17:48:17 +0300

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_puts - Entry point
+ *
+ *Description:  prints a string followed by a new line
+ *
+ *Return: nothing(successful)
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	_putchar('\n');
+}
