1a943648be25db437ac1a0a4ebd6026b8b4f8cc7
GreyShinobi2013 <getachewnatnael55@gmail.com>
2022-12-20 03:26:05 +0300

    Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,12 +3,12 @@
 /**
  * reset_to_98 - Entry point
  *
- * Description: reset value to 98
+ * Description: 'n' reset value to 98
  *
  * Return: Nothing (Success)
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;/*change value of pointer to 98*/
+	*n = 98;
 }
