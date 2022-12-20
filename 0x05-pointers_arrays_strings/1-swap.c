197ef50c11def4cdece2d921739433eee0eeb4aa
GreyShinobi2013 <getachewnatnael55@gmail.com>
2022-12-20 17:23:56 +0300

    Swap value of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
  *swap_int - Entry point
  *
  *@a: first input
  *@b: Second input
  *
- * Return - Nothing 
+ * Return - Nothing
  */
 
 void swap_int(int *a, int *b)
 {
 	int temp;
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
