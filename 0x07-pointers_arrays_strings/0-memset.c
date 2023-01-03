1c2d92a0151eed09d08bd18bc7ab0951b41537d5
GreyShinobi2013 <getachewnatnael55@gmail.com>
2023-01-03 04:40:03 +0300

    fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ *@s: starting address of memory to be filled
+ *@b: value to be filled
+ *@n: number of bytes to be filled
+ *
+ * Return: poiner.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return *memset(s, b, n);
+}
+
