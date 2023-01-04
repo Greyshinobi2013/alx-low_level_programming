b1752f0d4a3084052d4881ba9e86c67881cec8d3
GreyShinobi2013 <getachewnatnael55@gmail.com>
2023-01-04 03:55:13 +0300

    Add 1-memcpy and 2-strchr .c files

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ *
+ * @dest: first input
+ * @src: Second input
+ * @n: third input
+ *
+ * Return - char (success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return memcpy(dest,src,n);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ *
+ * @s: input_1
+ * @c: input_2
+ *
+ * Return - char(success)
+ *
+ **/
+char *_strchr(char *s, char c)
+{
+	return strchr(s,c);
+}
