06bd43f395f9add38277bcb60a141aff49a4b199
Greyshinobi2013 <getachewnatnael55@gmail.com>
2023-11-16 03:29:22 +0300

    Print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *_puts_recursion - Prints out String
+ *
+ *@s: String input
+ *
+ *Description: Takes in string parameter and print out String
+ *
+ *Return: none
+ */
+void _puts_recursion(char *s)
+{
+	puts(*s);
+}
