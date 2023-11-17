b87f08c2dc046920803ac5e23513e6e2ddfa99eb
Greyshinobi2013 <getachewnatnael55@gmail.com>
2023-11-17 05:12:31 +0300

    Identify if input is odd or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *is_prime_number - Returns 1 if the input integer is a prime number
+ *
+ *@n: Int parameter
+ *
+ *Description: Returns 1 if the input is a prime number, otherwise return 0
+ *
+ *Return: Int value
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
