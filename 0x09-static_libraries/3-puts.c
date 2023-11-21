7e83338519c4e6fde9812e9687be26ccdefe217b
Greyshinobi2013 <getachewnatnael55@gmail.com>
2023-11-22 00:55:57 +0300

    Edit 3-puts.c file

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,20 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ *_puts - Print string
+ *
+ *@s: String parameter
+ *
+ *Return: Nothing
+ */
 
 void _puts(char *s)
 {
- 	_putchar(*s);
+	int length = strlen(s);
+
+	for (int i = 0; i < length; i++)
+	{
+		_putchar(s);
+	}
 }
