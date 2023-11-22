79a88c0145a44e7688ad8ecd947278bacab999df
Greyshinobi2013 <getachewnatnael55@gmail.com>
2023-11-22 06:22:59 +0300

    Print minimum number of coins to make a change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *main - print argv and argc
+ *main - print minimum number of change for given cents
  *
  *@argc: Number of argument
  *@argv: Array of string
  *
- *Description: Print the name of the file
+ *Description: Print prints the minimum number of coins to make change
  *
- *Return: Zero
+ *Return: if sucessful(0) if not sucessful(1)
  */
 
 int main(int argc, char *argv[])
@@ -17,18 +17,19 @@ int main(int argc, char *argv[])
 	int i;
 	int coins[] = {25, 10, 5, 2, 1};
 	int num_coins = 0;
-	int cents = atoi(argv[1]);
+	int cents;
 
-	if (argc != 2)
+	if (argc != 2 || argc == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
-	printf("0\n");
-	return (0);
+		printf("0\n");
+		return (0);
 	}
 
 	for (i = 0; i < 5; i++)
