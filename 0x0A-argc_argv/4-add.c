fa71c438589715521e5795703448a9705fd4aef5
Greyshinobi2013 <getachewnatnael55@gmail.com>
2023-11-22 04:48:17 +0300

    Print addition of numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Print sum of posetive numbers
+ *
+ *@argc: Number of argument
+ *@argv: Array of string
+ *
+ *Description: Print addition of two posetive numbers
+ *
+ *Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int sum = 0,i;
+	
+	if (argc == 1)
+	{
+		printf("%d\n", sum);
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{	
+			if (atoi(argv[i]) != 0)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n",sum);
+	}
+	return (0);
+}
