78942e88cc45a175b43f54a50e823cf9dab0196e
Greyshinobi2013 <getachewnatnael55@gmail.com>
2023-11-26 15:49:23 +0300

    Concatinate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,44 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- *_strdup - Return a pointer to a newly allocated space in memory
+ * str_concat - Concatinate two strings
  *
- *@str: String parameter
+ *@s1: String parameter
+ *@s2: String parameter
  *
- *Description: Create and initialize char array
+ *Description: Concatinate two strings
  *
- *Return: character pointer
+ *Return: Concatinated string
  */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, sizeS1, sizeS2;
+	char *copyS1, *copyS2;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	sizeS1 = strlen(s1) + 1;
+	sizeS2 = strlen(s2) + 1;
+
+	copyS1 = malloc(sizeS1 + sizeS2);
+	copyS2 = malloc(sizeS2);
+
+	for (i = 0; i < sizeS1; i++)
+	{
+		copyS1[i] = s1[i];
+	}
+
+	for (j = 0; j < sizeS2; j++)
+	{
+		copyS2[j] = s2[j];
+	}
+
+	strcat(copyS1, copyS2);
+	return (copyS1);
+}
