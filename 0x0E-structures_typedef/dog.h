74bd67059cab8d42ec452ad39b7a10a236a40efd
Greyshinobi2013 <getachewnatnael55@gmail.com>
2023-11-29 04:04:01 +0300

    Define new datatype struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog - Define dog datatype
+ *
+ *@name: String datatype
+ *@age: Float datatype
+ *@owner: String datatype
+ *
+ *Description: Define new datatype 'dog' with three members
+ */
 struct dog
 {
 	char *name;
